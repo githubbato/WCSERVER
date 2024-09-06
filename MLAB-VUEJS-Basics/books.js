513b9f34ca676a8849f7255e969db86d960f4332
mikejr <141400798+githubbato@users.noreply.github.com>
2024-09-07 00:01:24 +0800

    Add files via upload

---
+++ MLAB-VUEJS-Basics/books.js
@@ -0,0 +1,17 @@
+const books = Vue.createApp({
+        data(){
+            return {
+            showBooks: true,
+            btitle: 'The Ledger',
+            bauthor: 'Joseph Esquivel',
+            year: 2008
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+}
+);
+books.mount('#books')
---
+++ MLAB-VUEJS-Basics/twoway.js
@@ -0,0 +1,9 @@
+const TwoWayBinding = {
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}
+
+Vue.createApp(TwowayBinding).mount('#two-way-binding')
