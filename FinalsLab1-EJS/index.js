5b932ff76498aec326b658852f38798c4356f258
mikejr <141400798+githubbato@users.noreply.github.com>
2024-09-21 21:32:06 +0800

    Add files via upload

---
+++ FinalsLab1-EJS/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'My EJS App' });
+});
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
+//serve static files
+app.use(express.static('public'));
